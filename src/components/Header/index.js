0cfb452f68c02fecdac2f8449731aa7d351cbdcd
Hiago Souza <hiagofss98@gmail.com>
2019-07-16 20:32:11 -0300

    Create and Styled Home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { MdShoppingBasket } from 'react-icons/md';
+
 import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
@@ -10,7 +11,7 @@ export default function Header() {
   return (
     <Container>
       <Link to="/">
-        <img src={logo} alt="Rocktshoes" />
+        <img src={logo} alt="Rocketshoes" />
       </Link>
 
       <Cart to="/cart">
